167ae140cd7f0c9e87a7549db45f8ae423fd1d7a
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2018-03-30 15:13:31 +1300

    fix some rubocop offences

--- lib/dual/configuration_methods.rb
+++ lib/dual/configuration_methods.rb
@@ -12,12 +12,5 @@ module Dual
     def add_association(*properties)
       properties.each { |prop| associations << prop }
     end
-
-    # Think about whether we need this
-    #%w[excludes includes add_association].each do |method|
-    #  define_method(method.to_sym) do |*properties|
-    #
-    #  end
-    #end
   end
 end
