7270e6d97cbc4806aab578fe6d904b76b97ae070
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2018-06-13 14:14:56 +1200

    add tests for one to one association

---
+++ spec/associations/one_to_one_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+RSpec.describe Dual::Associations::OneToOne do
+  subject { described_class }
+  let(:association) { 'association' }
+  let(:original_object) do
+    Class.new { attr_accessor :association }.new
+  end
+  let(:reflection) { Hash[:name, association] }
+  let(:dual_object) { original_object.dup }
+  let(:base) { subject.new(original_object, dual_object, reflection) }
+
+  describe '#copy' do
+    it 'copies the association to the dupped object' do
+      expect(original_object).to receive(:association)
+      expect(dual_object).to receive("#{association}=").twice
+
+      base.copy
+    end
+  end
+
+  describe '#remove' do
+    it 'removes the association from the dupped object' do
+      expect(dual_object).to receive("#{association}=").with(nil)
+
+      base.remove
+    end
+  end
+end
+
